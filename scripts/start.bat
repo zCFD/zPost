42d19934483d20038e3eaeea558d3b92c6a07802
mike-jt79 <mike.turner@zenotech.com>
2015-12-15 16:27:49 +0000

    assume anaconda or similar python env is available for windows

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,18 +1,10 @@
 @ECHO OFF
 echo "zPost Starting IPython notebook"
 
-call ..\zpost-py27\Scripts\activate
+call .\pv-location.bat
 
-call ..\pvconnect-py27\pv-location.bat
-
-echo "%PARAVIEW_BIN_LOCATION%"
-set PATH=%PARAVIEW_BIN_LOCATION%;%PATH%
+set PATH=%PARAVIEW_BIN%;%PATH%
 set PYTHONPATH=..;%PARAVIEW_SITE_LIB%\site-packages;%PARAVIEW_SITE_LIB%\site-packages\vtk;%PYTHONPATH%
-
-set PATH=%CD%\..\zpost-py27\Scripts;%PATH%
-
-echo %PYTHONPATH% 
-echo %PATH%
 
 cd ..\ipynb
 ipython notebook
